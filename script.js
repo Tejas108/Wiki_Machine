6675dc36476ac4410044f99ccca30162e245cc74
Tejas <tejas.monteverdi@gmail.com>
2018-03-21 11:27:30 -0400

    styles. refactor error

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 $(function () {
+  var api = "https://en.wikipedia.org/w/api.php?origin=*&action=query&format=json&list=search&srsearch=";
+  var term = $('form').find('input').val();
+  var $error = $('.error');
+
+  $('button[type="button"]').click(function(){
+    $('ul').html('');
+  });
 
   $('form').submit(function (e, api, term) {
     e.preventDefault();
@@ -6,32 +13,37 @@ $(function () {
     var $input = $('form').find('input');
     var term = $input.val();
     var url = api + term;
-    var $error = $('.error');
-    if ($input.val() != '') {
+
+    if ($input.val() != '' || undefined) {
+      console.log($input.val());
       $.ajax({
         url: url,
         dataType: 'json',
         type: 'GET',
         success: function (data) {
+          $('ul').html('');
           var json = data.query.search;
           handleBuildList(json);
         }
       });
       $input.val('');
-      $error.hide();
     } else {
-      $error.show();
+      $input.attr('placeholder','Enter a search term');
     }
   });
 
   function handleBuildList(json) {
-    $ul = $('ul');
+    var $ul = $('ul');
     for (var i = 0; i < json.length; i++) {
       var link = "https://en.wikipedia.org/?curid=" + json[i].pageid;
       var snip = json[i].snippet.split(".", 1) + ".";
-      $ul.append('<a href="' + link + '"><li><h2>' + json[i].title + '</h2><p>' + snip + '</p></li></a>');
+      $ul.append('<li class="mdl-list__item"><a href="' + link + '"><p class="mdl-list__item-primary-content"><strong>' + json[i].title + '</strong></p><p>' + snip + '</p></a></li>');
+      $ul.children().hide();
     }
-  }
 
+    $('ul li').each(function(i){
+      $(this).delay(400*i).fadeIn(300);
+    });
+  }
 
 });
